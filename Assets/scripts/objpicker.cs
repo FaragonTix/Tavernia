33a702d2f1ef7029fb565421cb6eec523c21a365
FaragonTix <dac100@mail.ru>
2018-07-15 02:16:08 +0400

    Начата работа над GUI. Добавлен текст.

--- Assets/scripts/objpicker.cs
+++ Assets/scripts/objpicker.cs
@@ -2,21 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using cakeslice;
+using UnityEngine.UI;
 
 public class objpicker : MonoBehaviour {
   // public Outline cl1 = new Outline();
+    public GameObject tb;
     void Start()
     {
-        this.gameObject.GetComponent<Outline>().enabled = false;
+        this.gameObject.GetComponent<cakeslice.Outline>().enabled = false;
     }
     void OnMouseEnter()
     {
-        this.gameObject.GetComponent<Outline>().enabled = true;
+        this.gameObject.GetComponent<cakeslice.Outline>().enabled = true;
      //   cl1.eraseRenderer=true;
 
     }
     void OnMouseExit()
     {
-        this.gameObject.GetComponent<Outline>().enabled = false;
+        this.gameObject.GetComponent<cakeslice.Outline>().enabled = false;
+    }
+    void OnMouseDown()
+    {
+       tb.GetComponent<Text>().text = "Это бутылка";
+        // this.gameObject.SetActive(false);
     }
 }
