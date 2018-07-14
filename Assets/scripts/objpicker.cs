53f333e19aeb560966199e28f22c58c4257b7fe3
FaragonTix <dac100@mail.ru>
2018-07-14 22:20:51 +0400

    ouliner

---
+++ Assets/scripts/objpicker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using cakeslice;
+
+public class objpicker : MonoBehaviour {
+  // public Outline cl1 = new Outline();
+    void Start()
+    {
+        this.gameObject.GetComponent<Outline>().enabled = false;
+    }
+    void OnMouseEnter()
+    {
+        this.gameObject.GetComponent<Outline>().enabled = true;
+     //   cl1.eraseRenderer=true;
+
+    }
+    void OnMouseExit()
+    {
+        this.gameObject.GetComponent<Outline>().enabled = false;
+    }
+}
