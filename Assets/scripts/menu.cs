532e93814ffd77d4d304ff9ad382eed609eec588
FaragonTix <dac100@mail.ru>
2018-07-14 17:46:33 +0400

    contextmenu

---
+++ Assets/scripts/menu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour
+{
+    public GameObject Menu;
+    public bool z=false;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (z == false)
+            {
+                z = true;
+            }
+            else
+            {
+                z = false;
+            }
+
+                Menu.gameObject.SetActive(z);
+            }
+           
+        }
+    
+
+  public  void Exit()
+    {
+        Application.Quit();
+    }
+}
+
+       
+    
+
+  
+
